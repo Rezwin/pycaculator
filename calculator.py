89228c46ad147178de1625acef2946bbb0f3fccc
Rezwin Sait <rezwinbai.r@gmail.com>
2022-12-18 13:55:03 +0000

    Remainder feature added

--- calculator.py
+++ calculator.py
@@ -1,6 +1,6 @@
 num1 = int(input("Enter first number: "))
 num2 = int(input("Enter second number: "))
-operation = input("Enter the operation you would like to perform (+,-,x,/) : ")
+operation = input("Enter the operation you would like to perform (+,-,x,/,%) : ")
 
 if operation == "+":
     print(f"Adding the numbers : \n {num1} + {num2} = {num1+num2}")
@@ -10,4 +10,8 @@ elif operation == "x":
     print(f"Multiplying the numbers : \n {num1} x {num2} = {num1*num2}")
 elif operation == "/":
     print(f"Dividing the numbers : \n {num1} / {num2} = {num1/num2}")
+elif operation == "%":
+    print(f"Remainder of the numbers : \n {num1} % {num2} = {num1%num2}")
+else:
+    print("Invalid operation!")
 
