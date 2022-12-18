b70095d023647deadc2724ca9d527d9fc33e2575
Rezwin Sait <rezwinbai.r@gmail.com>
2022-12-18 13:49:56 +0000

    adding division feature

--- calculator.py
+++ calculator.py
@@ -1,11 +1,13 @@
 num1 = int(input("Enter first number: "))
 num2 = int(input("Enter second number: "))
-operation = input("Enter the operation you would like to perform (+,-,x) : ")
+operation = input("Enter the operation you would like to perform (+,-,x,/) : ")
 
 if operation == "+":
-    print(f"The sum of the numbers are: \n {num1} + {num2} = {num1+num2}")
+    print(f"Adding the numbers : \n {num1} + {num2} = {num1+num2}")
 elif operation == "-":
-    print(f"Subtracting the numbers are: \n {num1} - {num2} = {num1-num2}")
+    print(f"Subtracting the numbers : \n {num1} - {num2} = {num1-num2}")
 elif operation == "x":
-    print(f"Multiplying the numbers are: \n {num1} x {num2} = {num1*num2}")
+    print(f"Multiplying the numbers : \n {num1} x {num2} = {num1*num2}")
+elif operation == "/":
+    print(f"Dividing the numbers : \n {num1} / {num2} = {num1/num2}")
 
